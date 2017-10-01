16710204568dff81abdf882bb9edb0e35bf19fa7
sdrzymala <slawomirdrzymala@outlook.com>
2017-10-02 00:39:27 +0200

    Add database project. Add table FileContent and sp sp_InsertFileContent

---
+++ seequality_twitter_analysis/Database/dbo/Stored Procedures/sp_InsertFileContent.sql
@@ -0,0 +1,22 @@
+﻿
+
+
+CREATE PROCEDURE [dbo].[sp_InsertFileContent] 
+(
+	@FilePath VARCHAR(1000),
+	@FileSize [BIGINT],
+	@HTMLDocument [NVARCHAR](MAX)
+)
+AS 
+INSERT INTO [dbo].[FileContent]
+(
+	FilePath
+	,[FileSize]
+	,[HTMLDocument]
+)
+VALUES
+(
+	@FilePath,
+	@FileSize,
+	@HTMLDocument
+)
---
+++ seequality_twitter_analysis/Database/dbo/Tables/FileContent.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[FileContent] (
+    [FileContentID] INT            IDENTITY (1, 1) NOT NULL,
+    [FilePath]      VARCHAR (1000) NULL,
+    [FileSize]      BIGINT         NULL,
+    [HTMLDocument]  NVARCHAR (MAX) NULL
+);
+
