754b9c0383edce4c58f32ce6cb95221b5c8477c1
sdrzymala <slawomirdrzymala@outlook.com>
2017-10-02 01:03:32 +0200

    Add Tweet table and sp_InsertTweet procedure to database project

---
+++ seequality_twitter_analysis/Database/dbo/Stored Procedures/sp_InsertTweet.sql
@@ -0,0 +1,68 @@
+﻿
+CREATE PROCEDURE [dbo].[sp_InsertTweet] 
+(
+	@UserAddressName [VARCHAR](500),
+	@UserID [BIGINT],
+	@UserImagePath [VARCHAR](500),
+	@UserFullName [VARCHAR](500),
+	@UserTwitterName [VARCHAR](500),
+	@Date [VARCHAR](500),
+	@StatusPath [VARCHAR](500),
+	@DateTimeTitle [VARCHAR](500),
+	@ConversationID [BIGINT],
+	@OriginalDateTime [BIGINT],
+	@OriginalDateTimeMS [BIGINT],
+	@DateTime [DATETIME2](7),
+	@TweetText [VARCHAR](500),
+	@TweetLanguage [VARCHAR](500),
+	@TweetImagePath [VARCHAR](500),
+	@NumberOfReplies [INT],
+	@NumberOfRetweets [INT],
+	@NumberOFFavourites [INT],
+	@NumberOfErrorsDuringParsing [INT]
+)
+AS
+INSERT INTO [dbo].[Tweet]
+(
+	[UserAddressName]
+	,[UserID]
+	,[UserImagePath]
+	,[UserFullName]
+	,[UserTwitterName]
+	,[Date]
+	,[StatusPath]
+	,[DateTimeTitle]
+	,[ConversationID]
+	,[OriginalDateTime]
+	,[OriginalDateTimeMS]
+	,[DateTime]
+	,[TweetText]
+	,[TweetLanguage]
+	,[TweetImagePath]
+	,[NumberOfReplies]
+	,[NumberOfRetweets]
+	,[NumberOFFavourites]
+	,[NumberOfErrorsDuringParsing]
+)
+VALUES	
+(
+	@UserAddressName,
+	@UserID,
+	@UserImagePath,
+	@UserFullName,
+	@UserTwitterName,
+	@Date,
+	@StatusPath,
+	@DateTimeTitle,
+	@ConversationID,
+	@OriginalDateTime,
+	@OriginalDateTimeMS,
+	@DateTime,
+	@TweetText,
+	@TweetLanguage,
+	@TweetImagePath,
+	@NumberOfReplies,
+	@NumberOfRetweets,
+	@NumberOFFavourites,
+	@NumberOfErrorsDuringParsing
+)
---
+++ seequality_twitter_analysis/Database/dbo/Tables/Tweet.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[Tweet] (
+    [TweetID]                     INT            IDENTITY (1, 1) NOT NULL,
+    [UserAddressName]             VARCHAR (500)  NULL,
+    [UserID]                      BIGINT         NULL,
+    [UserImagePath]               VARCHAR (500)  NULL,
+    [UserFullName]                VARCHAR (500)  NULL,
+    [UserTwitterName]             VARCHAR (500)  NULL,
+    [Date]                        VARCHAR (500)  NULL,
+    [StatusPath]                  VARCHAR (500)  NULL,
+    [DateTimeTitle]               VARCHAR (500)  NULL,
+    [ConversationID]              BIGINT         NULL,
+    [OriginalDateTime]            BIGINT         NULL,
+    [OriginalDateTimeMS]          BIGINT         NULL,
+    [DateTime]                    DATETIME2 (7)  NULL,
+    [TweetText]                   NVARCHAR (500) NULL,
+    [TweetLanguage]               VARCHAR (500)  NULL,
+    [TweetImagePath]              VARCHAR (500)  NULL,
+    [NumberOfReplies]             INT            NULL,
+    [NumberOfRetweets]            INT            NULL,
+    [NumberOFFavourites]          INT            NULL,
+    [NumberOfErrorsDuringParsing] INT            NULL
+);
+
