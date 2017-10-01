25dc6fc8d122be67394d979ff83515aec4c81876
sdrzymala <slawomirdrzymala@outlook.com>
2017-10-01 20:49:03 +0200

    Add sample method execution

--- seequality_twitter_analysis/SampleApplication/Program.cs
+++ seequality_twitter_analysis/SampleApplication/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Libraries;
+
 namespace SampleApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string directory = @"C:\Users\sldr01\Desktop\FILES\posts\pl_microsoft_ignite_summary";
+            ParseTwitterData.ParseAllFilesFromDirectory(directory);
+
+
         }
     }
 }
