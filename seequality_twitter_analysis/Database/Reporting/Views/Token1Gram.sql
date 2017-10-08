4ebbdba331a1ead7752eed42f14bcc0eff7ceb12
sdrzymala <slawomirdrzymala@outlook.com>
2017-10-08 02:45:47 +0200

    Add basic reporting views

---
+++ seequality_twitter_analysis/Database/Reporting/Views/Token1Gram.sql
@@ -0,0 +1,15 @@
+﻿
+CREATE VIEW [Reporting].[Token1Gram]
+AS
+SELECT 
+      [TweetID]
+      ,[Token]
+      ,[TokenRootWord]
+      ,[IsEnglishWord]
+      ,[IsStopWord]
+      ,[IsNotEnglishWordAndNotStopWord]
+      ,[IsHashtag]
+      ,[IsAccountName]
+      ,[IsNumber]
+      ,[IsWebsiteUrl]
+  FROM [TextMining].[Token1Gram]
--- seequality_twitter_analysis/Database/Security/Reporting.sql
+++ seequality_twitter_analysis/Database/Security/Reporting.sql
@@ -1,3 +1,5 @@
 ﻿CREATE SCHEMA [Reporting]
     AUTHORIZATION [dbo];
 
+
+
