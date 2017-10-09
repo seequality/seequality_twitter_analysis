7d363c65eeb64febf175c1141e88d6c409486af1
sdrzymala <slawomirdrzymala@outlook.com>
2017-10-10 01:27:35 +0200

    Modify view to include the ranking for accounts and hashtags

--- seequality_twitter_analysis/Database/Reporting/Views/Hashtag.sql
+++ seequality_twitter_analysis/Database/Reporting/Views/Hashtag.sql
@@ -2,11 +2,22 @@
 
 CREATE VIEW [Reporting].[Hashtag] 
 AS 
+WITH cte AS 
+(
+	SELECT h.Hashtag, ROW_NUMBER() OVER(ORDER BY COUNT(*) desc) AS HashtagRanking
+	FROM TextMining.Tweet t (NOLOCK)
+	INNER JOIN [TextMining].[Hashtags] h (NOLOCK)
+		ON h.TweetID = t.TweetID
+	GROUP BY h.Hashtag
+)
 SELECT 
-      [TweetID]
-      ,[Hashtag]
-      ,[IsOnTheWhiteList]
-  FROM [TextMining].[Hashtags] (NOLOCK)
+       h.[TweetID]
+      ,h.[Hashtag]
+      ,h.[IsOnTheWhiteList]
+	  ,c.HashtagRanking
+  FROM [TextMining].[Hashtags] h (NOLOCK)
+  INNER JOIN cte c 
+	ON c.Hashtag = h.Hashtag
   WHERE TweetID IN 
 (
 	SELECT TweetID FROM Internal.DistinctTweet
