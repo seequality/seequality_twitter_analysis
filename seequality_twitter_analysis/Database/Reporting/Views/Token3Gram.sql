82b0c7f22fe7a7c3cfd440b5da3ff992692d6c40
sdrzymala <slawomirdrzymala@outlook.com>
2017-10-19 00:37:57 +0200

    Add database objects for the 3-gram analysis - table, view, store procedure to insert the data

---
+++ seequality_twitter_analysis/Database/Reporting/Views/Token3Gram.sql
@@ -0,0 +1,11 @@
+﻿
+CREATE VIEW [Reporting].[Token3Gram]
+AS
+SELECT 
+      [TweetID]
+      ,[Token]
+  FROM [TextMining].[Token3Gram] (NOLOCK)
+  WHERE TweetID IN 
+(
+	SELECT TweetID FROM Internal.DistinctTweet
+)
---
+++ seequality_twitter_analysis/Database/TextMining/Tables/Token3Gram.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [TextMining].[Token3Gram] (
+    [TokenID] INT            IDENTITY (1, 1) NOT NULL,
+    [TweetID] INT            NULL,
+    [Token]   NVARCHAR (500) NULL,
+    CONSTRAINT [PK_TextMining_Token3Gram] PRIMARY KEY CLUSTERED ([TokenID] ASC)
+);
+
